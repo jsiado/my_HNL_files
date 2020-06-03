fb8d0d8bf79a20b721f380c57e15a1c6d01676c7
Joaquin E Siado Castaneda <jsiado@huskers.unl.edu>
2020-06-03 10:59:15 -0500

    update slide for 200603

--- Macros/RHN_Ana_Try.C
+++ Macros/RHN_Ana_Try.C
@@ -24,14 +24,17 @@ void RHN_Ana_Try(const char *inputFile)
 
 	// Get pointers to branches used in this analysis
 	TClonesArray *branchParticle = treeReader->UseBranch("Particle");
-
+	float totP = 0;
+	int numpar = 0;
+	float fiW = 0;
+	int numpar2 = 0;
 	// Loop over all events
 	for(Int_t entry = 0; entry < numberOfEntries; ++entry)
 	{
 		// Load selected branches with data from specified event
 		treeReader->ReadEntry(entry);
     
-		if(entry>10) break;
+// 		if(entry>10) break;
     
 // 		cout<<"" <<endl;
 		cout<<"--------- New Event ---------" <<endl;
@@ -41,12 +44,33 @@ void RHN_Ana_Try(const char *inputFile)
 		for(Int_t i=0; i < branchParticle->GetEntriesFast(); i++)
 		{    
 			GenParticle *gen = (GenParticle*) branchParticle->At(i);
-// 			GenParticle *genM = (GenParticle*) branchParticle->At(gen->M1);
-// 			GenParticle *genM2 = (GenParticle*) branchParticle->At(gen->M2);
-			if (abs(gen->PID) == 24)// && abs(gen->M1) == 13)
+			GenParticle *genM = (GenParticle*) branchParticle->At(gen->M1);
+			
+			if (abs(gen->PID) == 13 && abs(genM->PID) == 66)
 			{
-				cout<<" PID: "<<gen->PID<<", M1: "<<genM->PID<<", M2: "<<gen->M2<<", D1: "<<gen->D1<<", D2: "<<gen->D2<<endl;
+				numpar++;
+// 				cargar el momemtun:
+// 				comentar la linea
+// 				cout<<" PID: "<<gen->PID<<", M1: "<<genM->PID<<endl;
+				GenParticle *daug1 = (GenParticle*) branchParticle->At(genM->D1);
+				GenParticle *daug2 = (GenParticle*) branchParticle->At(genM->D2);
+				if (abs(daug1->PID) == 13 && abs(daug2->PID) == 14)
+				{
+					totP = totP + daug1->PT+daug2->PT;
+					cout<<" PID: "<<gen->PID<<", M1: "<<genM->PID<<", D1 "<<daug1->PID<<", D2: "<<daug2->PID<<" Pt "<<daug1->PT+daug2->PT<<" ";
+				}
+				
+				GenParticle *Gr = (GenParticle*) branchParticle->At(genM->M1);
+				if (abs(Gr->PID) == 24)//13 && abs(daug2->PID) == 14)
+				{
+					fiW = fiW + Gr->PT + daug1->PT + daug2->PT;
+					numpar2++;
+					cout<<Gr->PID<<endl;
+				}
+				cout<<endl;
 			}
 		}
+// 		cout<<numpar<<endl;
+		cout<<totP/numpar<<"  "<<fiW/numpar2<<endl;
 	}
 }
